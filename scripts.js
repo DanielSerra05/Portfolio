4175ae91de1045900974775e507952fe574591bc
Daniel Serra <danielcserra05@gmail.com>
2024-10-23 00:04:30 +0100

    use local storage to save user preferences, improve menu function name

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
-const red = "238, 61, 64",
-  blue = "30,144,255",
-  yellow = "255,215,0",
-  cyan = "0,255,255",
-  pink = "252,15,192";
+function setThemeColor(color) {
+  const red = "238, 61, 64",
+    blue = "30,144,255",
+    yellow = "255,215,0",
+    cyan = "0,255,255",
+    pink = "252,15,192";
 
-function changeTheme(color) {
   switch (color) {
     case "red":
       document.documentElement.style.setProperty("--primary-color", red);
@@ -25,8 +25,20 @@ function changeTheme(color) {
       break;
   }
 }
+function changeTheme(color) {
+  localStorage.setItem("theme", color);
 
-function menu() {
+  setThemeColor(color);
+}
+
+function toggleMenu() {
   const btnMenu = document.querySelector("body");
   btnMenu.classList.toggle("sidebar-open");
 }
+
+function setUserColor() {
+  var color = localStorage.getItem("theme");
+  setThemeColor(color);
+}
+
+setUserColor();
